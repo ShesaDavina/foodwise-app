3c4fc41c74102536f4d18ded778e2bc99509760f
lunettalatisha <lunettalatishaadzrallyanugraha@smkwikrama.sch.id>
2026-04-03 23:18:50 +0700

    Add Signup page component

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+
+function Signup() {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className="container">
+
+      <h1 className="title">Welcome!</h1>
+
+      <div className="card">
+
+        <form onSubmit={handleSubmit}>
+
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+
+          <label>Email Address</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="email@gmail.com"
+            value={formData.email}
+            onChange={handleChange}
+          />
+
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+
+          <button className="signupBtn">Sign Up</button>
+
+        </form>
+
+        <div className="divider">
+          <span>Or</span>
+        </div>
+
+        <button className="googleBtn">
+          <img
+            src="https://www.svgrepo.com/show/475656/google-color.svg"
+            alt="google"
+          />
+          Continue with Google
+        </button>
+
+        <p className="loginText">
+          Already have an account? <a href="#">Login</a>
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Signup;
